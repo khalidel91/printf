8068dcbf1bdd5c83ae9bc0a0c883280d629df73e
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-25 17:52:24 -0700

    print f

--- main.h
+++ main.h
@@ -8,10 +8,10 @@
 #include <string.h>
 
 int _printf(const char *format, ...);
-int case_cs(int i, va_list arg, int *num_prt_char);
+int case_cs(int i, va_list arg, int *count);
 char *int_to_base(int num, int base);
-int case_di(int i, va_list arg, int *num_prt_char);
-int handle_binary(int i, va_list arg, int *num_prt_char);
+int case_di(int i, va_list arg, int *count);
+int casebin(int i, va_list arg, int *count);
 
 
 #endif /*MAIN_H*/
