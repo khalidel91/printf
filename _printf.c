d08b896daf0b5ec968ae0bcd5f002457828f844e
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-26 16:41:03 -0700

    add d and i specifiers

--- _printf.c
+++ _printf.c
@@ -10,68 +10,116 @@
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
+}
+
+/**
+ * caseint - convert integer to string
+ * @num: the integer to convert
+ * @str: a buffer to store the resulting string
+ * Return: the length of the resulting string
+ */
+int caseint(int num, char *str)
+{
+    int i = 0, sign = 0;
+    if (num == 0)
+    {
+        str[i++] = '0';
+    }
+    else if (num < 0)
+    {
+        sign = 1;
+        num = -num;
+    }
+    while (num != 0)
+    {
+        str[i++] = num % 10 + '0';
+        num = num / 10;
+    }
+    if (sign)
+    {
+        str[i++] = '-';
+    }
+    str[i] = '\0';
+    int len = i;
+    for (i = 0; i < len / 2; i++)
+    {
+        char tmp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = tmp;
+    }
+    return len;
 }
 /**
  * _printf - function printf
  * @format: the format of arguments
  * Return: the numberof printed charachetrs
  */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, res = 0;
-    char *str;
-    va_start(args, format);
+	va_list args;
+	int i, res = 0;
+	char *str;
 
-    if (format == NULL)
-    {
-	    return -1;
-    }
-    for (i = 0; format && format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
+	va_start(args, format);
 
-            if (format[i] == 'c')
-            {
-                _putchar(va_arg(args, int));
-                res++;
-            }
-            else if (format[i] == 's')
-            {
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    _putchar(*str);
-                    str++;
-                    res++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                _putchar('%');
-                res++;
-            }
-
-	else if (!format[i])
+	if (format == NULL)
 	{
-		i--;
+		return (0);
 	}
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			if (format[i] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				res++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					_putchar(*str);
+					str++;
+					res++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				_putchar('%');
+				res++;
+			}
+			else if (!format[i])
+			{
+				i--;
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+			{
+				int num = va_arg(args, int);
+				char buffer[20];
+				int len = caseint(num, buffer);
+				for (int j = 0; j < len; j++)
+				{
+					_putchar(buffer[j]);
+					res++;
+				}
+			}
+
+		}
+
+		else
+		{
+			_putchar(format[i]);
+			res++;
+		}
 	}
-		
-        else
-        {
-            _putchar(format[i]);
-            res++;
-        }
-    }
-
-    va_end(args);
-
-    return (res);
+	va_end(args);
+	return (res);
 }
-    
